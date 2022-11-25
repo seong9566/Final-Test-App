3012cf56180f23bbc2d6237a92eb6e1ed0db37fe
seong9566 <dlgustjd9566@gmail.com>
2022-11-25 10:22:44 +0900

    store_app 간단하게 완료

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # final_test_app
 - Final_Project 진행에 필요한 공부를 위한 저장소
 - 기술마다 브랜치로 남기기
-- 참고할 사이트 : pub_dev, https://docs.flutter.dev/reference/widgets
-
 ## 공부 필요한 위젯
 1. nestedscrollview
 2. bottom sheet (bottom navigation은 버튼 클릭시 화면 이동에만 사용됨.)
 3. 사용자에게 이미지 입력 받고 Base-64로 인코딩해서 넘겨주기
 4. 요일 입력받는 Calender 라이브러리 찾아서 사용
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StorePage(),
+    );
+  }
+}
+
+// stl 이라고 적으면 자동완성 기능이 활성화 된다.
+class StorePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  Text("Woman", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Kids", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Shoes", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Bag", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            Expanded(child: Image.asset("assets/naver.jpeg", fit: BoxFit.cover)),
+            SizedBox(height: 2),
+            Expanded(child: Image.asset("assets/cloth.jpeg", fit: BoxFit.cover)),
+          ],
+        ),
+      ),
+    );
   }
 }
