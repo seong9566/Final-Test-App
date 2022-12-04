9843417172636e03ab10d4f2a42610ccb54e11e6
seong9566 <dlgustjd9566@gmail.com>
2022-12-04 20:24:12 +0900

    별점 리뷰 테스트 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: RatingBar.builder(
+            initialRating: 3,
+            minRating: 0,
+            direction: Axis.horizontal, // 가로 정렬
+            allowHalfRating: true, // 0.5점 단위 : true, 1점 단위 false
+            itemCount: 5, // 리뷰의 별점 갯수
+            itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+            itemBuilder: (context, _) => Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+            onRatingUpdate: (rating) {
+              print(rating);
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
