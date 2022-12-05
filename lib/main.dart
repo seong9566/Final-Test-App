aa6335a44d5ab50b5cba80094ca9e75309eb1704
seong9566 <seong9566@github.com>
2022-12-05 09:58:36 +0900

    기본 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text("MainPage")),
+      ),
+    );
   }
 }
